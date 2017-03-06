319f9e93f40465ef8002232d97d3429005e70b98
Jerry Huang <jerry.huang@jerrynet.xyz>
2017-03-07 01:07:42 +0800

    BLE Scan

--- UWP/OpenTrackBLEBridge/OpenTrackBLEBridge/MainPage.xaml.cs
+++ UWP/OpenTrackBLEBridge/OpenTrackBLEBridge/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -13,6 +14,12 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
+using Windows.Devices.Bluetooth.Advertisement;
+using Windows.Devices.Bluetooth.GenericAttributeProfile;
+using Windows.Devices.Enumeration;
+using Windows.Devices.Bluetooth;
+using System.Diagnostics;
+
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace OpenTrackBLEBridge
@@ -22,9 +29,50 @@ namespace OpenTrackBLEBridge
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        public ObservableCollection<BLEDevice> BLEDevices { get; set; } = new ObservableCollection<BLEDevice>();
+
+        BluetoothLEAdvertisementWatcher bleADWatvher = new BluetoothLEAdvertisementWatcher();
+        
         public MainPage()
         {
             this.InitializeComponent();
+            bleADWatvher.Received += BleADWatvher_Received;
+            bleADWatvher.Start();
         }
+
+        private async void BleADWatvher_Received(BluetoothLEAdvertisementWatcher sender, BluetoothLEAdvertisementReceivedEventArgs args)
+        {
+            await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+            {
+                var exists = BLEDevices.Where(b => b.BluetoothAddress == args.BluetoothAddress).ToList();
+                if (exists.Count == 0)
+                {
+                    BLEDevices.Add(new BLEDevice
+                    {
+                        Name = string.IsNullOrWhiteSpace(args.Advertisement.LocalName) ? "N/A" : args.Advertisement.LocalName,
+                        Rssi = args.RawSignalStrengthInDBm,
+                        BluetoothAddress = args.BluetoothAddress
+                    });
+                    
+                }
+                else
+                {
+                    exists.ForEach(delegate (BLEDevice e)
+                    {
+                        e.BluetoothAddress = args.BluetoothAddress;
+                        e.Name = string.IsNullOrWhiteSpace(args.Advertisement.LocalName) ? "N/A" : args.Advertisement.LocalName;
+                        e.Rssi = args.RawSignalStrengthInDBm;
+                    });
+                }
+            });
+        }
+    }
+
+    public class BLEDevice
+    {
+        public string Name { get; set; }
+        public System.Int16 Rssi { get; set; }
+        public System.UInt64 BluetoothAddress { get; set; }
+        public string BluetoothAddressHex { get { return BluetoothAddress.ToString("X2"); } }
     }
 }
